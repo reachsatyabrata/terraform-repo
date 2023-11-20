0bf8e4d2e9c3f6b5a7cb7eb72019768f3ccf3892
reachsatyabrata <43322470+reachsatyabrata@users.noreply.github.com>
2023-11-20 14:57:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_vpc" "web-vpc" {
-  cidr_block = "10.4.0.0/16"
+resource "aws_vpc" "vpc-05f4c6e3d31691dec" {
+  cidr_block = "172.31.0.0/16"
   instance_tenancy = "default"
   tags = {
     Name = "Test-VPC"
